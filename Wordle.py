d6c7b25edda904a215c5b1c5189141c6e8247dc9
McBxby <mlcrandall@willamette.edu>
2024-09-24 13:07:15 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,8 +1,8 @@
 ########################################
-# Name:
+# Name: Myles Crandall
 # Collaborators (if any):
 # GenAI Transcript (if any):
-# Estimated time spent (hr):
+# Estimated time spent (hr): 0.75
 # Description of any added extensions:
 ########################################
 
@@ -12,10 +12,27 @@ import random
 
 def wordle():
     # The main function to play the Wordle game.
+    current_row = 0
+    N_ROWS = 6
 
     def enter_action():
         # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
+        # Get the current guess from the current row
+        guess = word_from_row(current_row).upper()
+
+        if len(guess) == N_COLS and is_english_word(guess.lower()):
+            gw.show_message("That's a 5 letter English word")  # Show message for correct guess
+        else:
+            gw.show_message("Not in word list")  # Show message for invalid words
+
+
+    def word_from_row(row):
+        # Get the word from a specific row
+        word = ""
+        for col in range(N_COLS):
+            letter = gw.get_square_letter(row, col)
+            word += letter
+        return word
 
 
     gw = WordleGWindow()
